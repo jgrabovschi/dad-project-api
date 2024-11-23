61849cf5c75689bc05995693f7b7d3557c0ecd0c
jgrabovschi <iorgusor@gmail.com>
2024-11-23 12:44:05 +0000

    Board API, get and post

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,16 @@
 <?php
 
+use App\Http\Controllers\api\BoardController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+//************
+// Boards API
+//************
+
+Route::get('/boards', [BoardController::class, 'index']);
+Route::post('/boards', [BoardController::class, 'store']);
